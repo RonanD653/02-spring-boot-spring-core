f13b8868e8a6a96549a418c967594c0315e5616c
RonanD653 <Ronan_D@Live.co.uk>
2024-06-13 15:33:05 +0100

    test

---
+++ src/test/java/com/udemyspringboot/__spring_boot_spring_core/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemyspringboot.__spring_boot_spring_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
