a1ea46c3678aa86fbe05b8838e9f7b4c80c5182a
RonanD653 <Ronan_D@Live.co.uk>
2024-06-13 17:02:37 +0100

    Constructor Injection

---
+++ src/main/java/com/udemyspringboot/__spring_boot_spring_core/DemoController.java
@@ -0,0 +1,24 @@
+package com.udemyspringboot.__spring_boot_spring_core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.PrivateKey;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
